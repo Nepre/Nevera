cb2665f9e5b03f0ee0b8f487cd5cf04bfc5c2a02
Raquel <raquelcb98@gmail.com>
2020-05-19 20:33:04 +0200

    Cositas de la shopping list

--- functions.js
+++ functions.js
@@ -44,6 +44,15 @@ function alternarAmbasLuces() {
 	frigo.congeladorLuz = !frigo.congeladorLuz;
 }
 
+function deleteElement(id){
+	$(id).hide();
+}
+
+function addElement(){
+	$(".new-tag").show();
+	$(".new-tag").html("<div class='new-tag m-1 text-xs flex inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-purple-300 text-gray-700 rounded-full'><p>Pan</p><span title='Eliminar' class='fas fa-times cursor-pointer px-1 py-2 fill-current text-grey'></span></div>");
+}
+
 setInterval(function(){
 	var date = new Date();
 	setTime(date);
@@ -63,11 +72,11 @@ function setTime(dateobj){
 // Menu de hamburguesa
 $(document).ready(function(){
 	$(".fa-bars").click(function(){
-		$("nav").toggle();
+		$(".burger").toggle();
 	});
 });
 
-/* SETTINGS */
+/* SETTINGS & SHOPPING LIST */
 
 // Para cambiar el titulo de los settings dependiendo de donde pinches
 $(document).ready(function(){
@@ -77,3 +86,33 @@ $(document).ready(function(){
 });
 
 /************/
+
+function showShoppingList(){
+	let title = "<h2 class='text-4xl w-full mb-2 z-40'>Lista de la compra</h2>";
+	let shoppingItems = "<div class='shopping-list flex flex-col w-auto'>" +
+		"<div class='rounded-lg flex bg-gray-200 my-2'>" +
+			"<img src='resources/egg.png' alt='Huevos' class='h-32 w-32 rounded-l-lg object-cover'>" +
+			"<div class='data flex-col w-full'>" + 
+				"<h3 class='px-4 py-2 text-2xl'>Docena de huevos</h3>" +
+				"<h4 class='px-4'>Precio: 0.95€</h4>" +
+			"</div>" +
+			"<div class='bg-purple-300 selector flex flex-col justify-center rounded-r-lg w-12'>" +
+				"<button class='my-auto'><span class='fas fa-plus'></span></button>" +
+				"<input type='number' name='Cantidad' value='1' class='bg-purple-100 h-10 text-center pl-3 font-bold'>" +
+				"<button class='my-auto'><span class='fas fa-minus'></span></button>" +
+			"</div>" +
+		"</div>" +
+	"</div>";
+
+	for(let i = 0; i < 6; i++)
+		title += shoppingItems;
+	
+	$(".principal").html(title);
+	
+	return false;
+}
+
+function showFoodItems(id){
+	$(".principal").html("<h2 class='text-4xl w-full mb-2 z-40'>" + $(id).text() + "</h2>");
+	return false;
+}

c1fdacb7d6c76b3b6ade7de39f40e4844d866e52
Nepre <edugominola@gmail.com>
2020-06-04 16:39:14 +0200

    Charts super molonas
    Con chartist
    Solo 3 archivos
    Los que hacen falta
    No más
    no menos

--- html4settings.js
+++ html4settings.js
@@ -51,3 +51,29 @@ var ajustesGenerales = `
                                 </h4> 
                             </div>
 `
+
+var timedate = `
+<!--Deteccion automatica-->
+<div class="flex pt-1"> 
+   <div class="pr-3 pt-2">
+       <label for="checked" class="inline-flex items-center cursor-pointer">
+           <span class="relative">
+             <span class="block w-10 h-6 bg-gray-400 rounded-full shadow-inner"></span>
+             <span id="inputDetect" class="absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out bg-purple-400 transform translate-x-full">
+               <input id="checked" onclick="changeInput('inputDetect', this.checked); changeDetection(this.checked);" type="checkbox" class="absolute opacity-0 w-0 h-0" checked/>
+             </span>
+           </span> 
+       </label>
+   </div>
+   <h4 class="text-2xl">
+       <span class="mb-16">Obtener fecha y hora de Internet</span>
+   </h4>
+   <br>
+
+   <div>
+       <input id="timeInput" type="time" class="border-2 border-gray-300 rounded-lg p-2 w-full my-2">
+       <input id="dateInput" class="justify-start border-2 border-gray-300 rounded-lg p-2 w-full my-2" type="date">
+   </div>
+   
+</div>
+`
